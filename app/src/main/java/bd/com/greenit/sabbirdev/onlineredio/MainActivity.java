543577629879c18eb84c7f5e80c7c663e636bafe
Sabbir Ahmed <sabbir.developer@gmail.com>
2015-08-31 21:21:40 +0600

    Initial commit

---
+++ app/src/main/java/bd/com/greenit/sabbirdev/onlineredio/MainActivity.java
@@ -0,0 +1,145 @@
+package bd.com.greenit.sabbirdev.onlineredio;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.IOException;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnBufferingUpdateListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+
+
+    private ProgressBar playSeekBar;
+
+    private Button buttonPlay;
+
+    private Button buttonStopPlay;
+
+    private MediaPlayer player;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        initializeUIElements();
+
+        initializeMediaPlayer();
+    }
+
+    private void initializeUIElements() {
+
+        playSeekBar = (ProgressBar) findViewById(R.id.progressBar1);
+        playSeekBar.setMax(100);
+        playSeekBar.setVisibility(View.INVISIBLE);
+
+        buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+
+        buttonStopPlay = (Button) findViewById(R.id.buttonStopPlay);
+        buttonStopPlay.setEnabled(false);
+        buttonStopPlay.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        if (v == buttonPlay) {
+            startPlaying();
+        } else if (v == buttonStopPlay) {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        buttonStopPlay.setEnabled(true);
+        buttonPlay.setEnabled(false);
+
+        playSeekBar.setVisibility(View.VISIBLE);
+
+        player.prepareAsync();
+
+        player.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mp) {
+                player.start();
+            }
+        });
+
+    }
+
+    private void stopPlaying() {
+        if (player.isPlaying()) {
+            player.stop();
+            player.release();
+            initializeMediaPlayer();
+        }
+
+        buttonPlay.setEnabled(true);
+        buttonStopPlay.setEnabled(false);
+        playSeekBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void initializeMediaPlayer() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource("http://server.realitsolution.com:5128/");
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        player.setOnBufferingUpdateListener(new OnBufferingUpdateListener() {
+
+            public void onBufferingUpdate(MediaPlayer mp, int percent) {
+                playSeekBar.setSecondaryProgress(percent);
+                Log.i("Buffering", "" + percent);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player.isPlaying()) {
+            player.stop();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
